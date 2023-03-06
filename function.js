e23f1031623e028be13cde6edb8a9af1fa3f6745
Robermendez <127061596+Robermendez@users.noreply.github.com>
2023-03-05 22:33:14 -0300

    Add files via upload

---
+++ function.js
@@ -0,0 +1,5 @@
+document.addEventListener('click', function() {
+    console.log("Mi Remuneracion Pretendida es de $ 490.000");
+    document.getElementById("demo").innerHTML="Mi remuneracion pretendida es de $ 490.000"
+});
+
---
+++ function2.js
@@ -0,0 +1,4 @@
+document.getElementById('boton_2').addEventListener('click', function() {
+    console.log("Mi contacto es");
+    document.getElementById("demo2").innerHTML="Mi Celular es 15-3438-6209"
+});
